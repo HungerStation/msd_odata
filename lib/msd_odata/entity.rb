734ada641bd57150f122e02a81cf5dcafd66cc20
Khaled <khaled.j@hungerstation.com>
2020-06-11 07:20:50 +0300

    feat(query builder): implement OData query methods

--- lib/msd_odata/entity.rb
+++ lib/msd_odata/entity.rb
@@ -1,10 +1,182 @@
 module MsdOdata
   class Entity
-    attr_reader :name, :attrs
+    ##
+    # This class behaves as an entity model.
+    # And takes the entity name in plural and capitalized format as 'name' attribute, and 'attrs' to pass payload or options.
+    # Entity name should be the same as in the URI.
+    # ex: {{base_url}}/Customers
+    #     MsdOdata::Entity.new('Customers')
+    #
+    # This class also contains methods for 'OData system query options' to build a query.
+    # To find out more about each method refer to: https://msdn.microsoft.com/en-us/library/gg309461.aspx
+    attr_reader :name, :attrs, :query
 
-    def initialize(name, attrs = nil)
+    def initialize(name, attrs = {})
       @name = name
       @attrs = attrs
+      @query = {}
+    end
+
+    # Specifies associations to expand in the result.
+    # Usage example: account.expand('opportunity_customer_accounts')
+    # @param properties [Array<Symbol>]
+    # @return [self]
+    def expand(*props)
+      @query[:expand] = props.join(',')
+      self
+    end
+
+    # Specifies properties to order the result by.
+    # Usage example: entity.order_by(:Name, :id)
+    # @param properties [Array<Symbol>]
+    # @return [self]
+    def order_by(*props)
+      @query[:order_by] = props.join(',')
+      self
+    end
+
+    # Specifies properties to select within the result.
+    # @param properties [Array<Symbol>]
+    # @return [self]
+    def select(*props)
+      @query[:select] = props.join(',')
+      self
+    end
+
+    # Sets a number of records to skip before retrieving records in a collection.
+    # @param properties [Array<Symbol>]
+    # @return [self]
+    def skip(*props)
+      @query[:skip] = props.join(',')
+      self
+    end
+
+    # Adds limit criteria to query.
+    # @param value [to_i]
+    # @return [self]
+    def limit(value)
+      @query[:top] = value.to_i
+      self
+    end
+
+    # Adds a filter to the query.
+    #
+    # For example:
+    #   AccountId eq 'HS-112233'
+    #
+    # Usage example: entity.where(entity[:AccountId].eq('ID-12345'))
+    #
+    # Operators:
+    # eq, ne, gt, ge, lt, le, and, or, not
+    #
+    # @param expressions [Array<String>]
+    # @return [self]
+    def where(*exps)
+      joined_exps = exps.join(' and ')
+
+      if @query[:filters].nil?
+        @query[:filters] = joined_exps
+      else
+        @query[:filters] += " and #{joined_exps}"
+      end
+
+      self
+    end
+
+    # Adds the logical operator 'or' to the query and start another chain.
+    #
+    # Usage example:
+    #   entity.where(entity[:Type].eq('customer')).or(entity[:Id].eq('HS-XXXX'))
+    #
+    # @param expressions [Array<String>]
+    # @return [self]
+    def or(*exps)
+      joined_exps = exps.join(' and ')
+      @query[:filters] += " or #{joined_exps}"
+      self
+    end
+
+    # Adds the logical operator 'and' to the query.
+    # @param expressions [Array<String>]
+    # @return [self]
+    def and(*exps)
+      joined_exps = exps.join(' and ')
+      @query[:filters] += " and #{joined_exps}"
+      self
+    end
+
+    # Adds the logical operator 'not' to the query.
+    # This should be only used within 'and' or 'or' operators.
+    #
+    # Usage example:
+    def not(*exps)
+      joined_exps = exps.join(' and ')
+      @query[:filters] += " not #{joined_exps}"
+      self
+    end
+
+    # Sets an instance variable to be used in a chained expression.
+    # Usage Example:
+    #   self[:x].eq(1)
+    #   This will generate the expression: x eq 1
+    #
+    # @param attr [to_s]
+    # @return self
+    def [](attr)
+      @attr = attr.to_s
+      self
+    end
+
+    # 'Equal to' expression
+    # @param val [*]
+    # @return [String]
+    def eq(val)
+      expression(:eq, val)
+    end
+
+    # 'Not Equal to' expression
+    # @param val [*]
+    # @return [String]
+    def ne(val)
+      expression(:ne, val)
+    end
+
+    # 'Greater Than' expression
+    # @param val [*]
+    # @return [String]
+    def gt(val)
+      expression(:gt, val)
+    end
+
+    # 'Greater Than or Equal' expression
+    # @param val [*]
+    # @return [String]
+    def ge(val)
+      expression(:ge, val)
+    end
+
+    # 'Less Than' expression
+    # @param val [*]
+    # @return [String]
+    def lt(val)
+      expression(:lt, val)
+    end
+
+    # 'Less Than or Equal' expression
+    # @param val [*]
+    # @return [String]
+    def le(val)
+      expression(:le, val)
+    end
+
+    private
+
+    # Builds the expression as a string statement.
+    # Note: Single qoutes on the value if it's a String or Symbol
+    # Example: "Attribute eq 'value'"
+    def expression(operator, val)
+      val = (val.is_a?(String) || val.is_a?(Symbol)) ? "'#{val}'" : val
+      "#{@attr} #{operator} #{val}"
     end
   end
 end
--- lib/msd_odata/url_helper.rb
+++ lib/msd_odata/url_helper.rb
@@ -5,14 +5,37 @@ module MsdOdata
       @entity = entity
     end
 
-    def entity_collection_url
-      "#{@base_url}/data/#{@entity.name}"
+    def entity_collection_url(with_query: false)
+      url = "#{@base_url}/data/#{@entity.name}"
+
+      return url unless with_query
+      url + build_query
     end
 
     def entity_url
-      raise "'url_params' are missing." unless @entity.attrs[:url_params].is_a? Hash
-      params = @entity.attrs[:url_params].map { |k,v| "#{k}='#{v}'"}.join(',')
-      "#{entity_collection_url}(#{params})"
+      raise "'url_params' are missing." unless @entity.attrs[:url_params].is_a?(Hash)
+
+      "#{entity_collection_url}(#{build_url_params})"
+    end
+
+    private
+
+    # Builds a query string from the query_hash in 'entity' instance
+    def build_query
+      query = '?'
+      query_hash = @entity.query
+      query += "$select=#{query_hash[:select]}&" unless query_hash[:select].nil?
+      query += "$filter=#{query_hash[:filters]}&" unless query_hash[:filters].nil?
+      query += "$orderby=#{query_hash[:order_by]}&" unless query_hash[:order_by].nil?
+      query += "$top=#{query_hash[:top]}&" unless query_hash[:top].nil?
+      query += "$expand=#{query_hash[:expand]}&" unless query_hash[:expand].nil?
+      query += "$skip=#{query_hash[:skip]}&" unless query_hash[:skip].nil?
+      query
+    end
+
+    # Parses 'url_params' hash to look like: "key='value',key='value'"
+    def build_url_params
+      @entity.attrs[:url_params].map { |k, v| "#{k}='#{v}'" }.join(',')
     end
   end
 end
